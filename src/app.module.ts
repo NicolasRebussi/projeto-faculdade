bd37a2e9bfbcecdacb006d5f817d46415ea4a878
NicolasRebussi <156813104+NicolasRebussi@users.noreply.github.com>
2025-02-28 12:28:57 -0300

    prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsuarioModule } from './usuario/usuario.module';
+import { UniversidadeModule } from './universidade/universidade.module';
+import { CursoModule } from './curso/curso.module';
 
 @Module({
-  imports: [UsuarioModule],
+  imports: [UsuarioModule, UniversidadeModule, CursoModule],
   controllers: [AppController],
   providers: [AppService],
 })
